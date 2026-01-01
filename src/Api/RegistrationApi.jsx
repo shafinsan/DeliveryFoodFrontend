63b73bc4e8fefa5cadb88e0d45d8e2403ed013ee
=elias jaber <eliasjabershafin599@gmail.com>
2026-01-01 18:31:48 +0600

    resolve registration

--- src/Api/RegistrationApi.jsx
+++ src/Api/RegistrationApi.jsx
@@ -3,7 +3,7 @@ const BASE_URL = "/api";
 export const Reg = async (data) => {
   try {
     
-    const response = await axios.post(`${BASE_URL}`, data);
+    const response = await axios.post(`${BASE_URL}/Registration/Registration`, data);
     return response.data;
   } catch (error) {
     throw new Error(
